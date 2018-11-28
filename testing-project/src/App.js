d3ebcec4bfd35681f7911c94dd09d75e0a8734b7
HaykYeghiazaryan <haykk.eghiazaryan@gmail.com>
2018-11-28 09:32:39 +0400

    added UsesInCompany CompaniesInUser CompanyProfile CompanyTests InvitedUsers pages desin

--- testing-project/src/App.js
+++ testing-project/src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import './App.css';
 import {Switch, Route} from "react-router-dom";
 import TestCreator from './containers/QuizCreator/TestCreator';
@@ -6,6 +7,9 @@ import AboutUs from './components/AboutUs/AboutUs';
 import Authorization from "./components/Autorization/Authorization";
 import AutorizationUser from "./components/Autorization/AutorizationUser";
 import AutorizationCompany from "./components/Autorization/AutorizationCompany";
+import Company from "./containers/Pages/Company";
+import CompaniesInUser from "./containers/Pages/CompaniesInUser";
+import UsersInCompany from "./containers/Pages/UsersInCompany";
 import AllTests from "./containers/Pages/AllTests";
 import AllCompanies from "./containers/Pages/AllCompanies";
 import AllUsers from "./containers/Pages/AllUsers";
@@ -14,6 +18,7 @@ import {firebase} from './firebase/firebase';
 import NoMatch from "./components/NoMatch";
 import HomePage from "./components/HomePage";
 import Header from "./components/Header";
+
 
 class App extends Component {
   render() {
@@ -28,8 +33,11 @@ class App extends Component {
             <Route path="/Companies/" component={AllCompanies} />
             <Route path="/Users/" component={AllUsers} />
             <Route path="/User/:Text" component={User} />
+            <Route path="/Company/:Text" component={Company} />
+            <Route path="/CompaniesInUser/" component={CompaniesInUser} />
+            <Route path="/UsersInCompany/" component={UsersInCompany} />
             <Route path="/aboutUs/" component={AboutUs} />
-            <Route path="/testCreater/" component={TestCreater} />
+            <Route path="/testCreator/" component={TestCreator} />
             <Route path="/testPassPanel/" component={TestPassPanel} />
             <Route path="/tests/" component={AllTests} />
             <Route component={NoMatch}/>
