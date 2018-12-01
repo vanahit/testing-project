bc6e69d4c0c8d593930857d4aa263835d10cfd7e
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2018-12-01 10:30:51 +0400

    header

--- testing-project/src/components/Header_footer/Header.js
+++ testing-project/src/components/Header_footer/Header.js
@@ -15,32 +15,40 @@ const Header = (props) => {
 
 
     return (
-        <header className='header'>
-            <NavLink to={'/'}>
-                <div className='logo'>LOGO</div>
-            </NavLink>
+        <header style={{
+            backgroundColor:'#141218',
+            width:'100%',
+        }}>
 
-            <div className='navigation'>
-                <NavLink to={'/tests'}>TESTS</NavLink>
-                <NavLink to={'/companies'}>COMPANIES</NavLink>
-                <NavLink to={'/users'}>USERS</NavLink>
-                <NavLink to={'/AboutUs'}>ABOUT US</NavLink>
-            </div>
+            <div className='header_wrapper'>
+                <NavLink to={'/'}>
+                    <div className='logo'>LOGO</div>
+                </NavLink>
 
-            {props.isLogged ?
-                <div style={{
-                    display: 'flex',
-                    alignItems: 'center'
-                }}>
-                    <NavLink
-                        style={{color: 'white', marginRight: '10px', border: '1px solid black', padding: '10px'}}
-                        to={'/companyPage'}>MY ACCOUNT</NavLink>
-                    <div className='header-log-in' onClick={() => logOut()}>LOG OUT</div>
+                <div className='navigation'>
+                    <NavLink to={'/tests'}>TESTS</NavLink>
+                    <NavLink to={'/companies'}>COMPANIES</NavLink>
+                    <NavLink to={'/users'}>USERS</NavLink>
+                    <NavLink to={'/AboutUs'}>ABOUT US</NavLink>
                 </div>
 
-                : <NavLink to={'/authorization'}>
-                    <div className='header-log-in'>LOG IN</div>
-                </NavLink>}
+                {props.currentLog ?
+                    <div style={{
+                        display: 'flex',
+                        alignItems: 'center'
+                    }}>
+                        <NavLink
+                            style={{color: 'white', marginRight: '10px', border: '1px solid black', padding: '10px'}}
+                            to={'/companyPage'}>MY ACCOUNT</NavLink>
+                        <div className='header-log-in' onClick={() => logOut()}>LOG OUT</div>
+                    </div>
+
+                    : <NavLink to={'/authorization'}>
+                        <div className='header-log-in'>LOG IN</div>
+                    </NavLink>}
+            </div>
+
+
         </header>
     )
 };
