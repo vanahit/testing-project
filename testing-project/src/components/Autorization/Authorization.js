358fc773c02c98d2063c0e4f0c548c62a7f30ea4
vanahit <anahitvardevanyan89@gmail.com>
2018-12-08 05:06:21 +0400

    autorization

--- testing-project/src/components/Autorization/Authorization.js
+++ testing-project/src/components/Autorization/Authorization.js
@@ -32,18 +32,6 @@ class Authorization extends Component {
             selectedTab: tab
         })
     }
-    // {(this.props.user && localStorage.getItem("current") === "company") ? 
-    //                 <Redirect to={`/${this.props.user.name}/profile`}/> :
-    //                 (this.props.user && localStorage.getItem("current") === "user") ?
-    //                 <Redirect to={`/${this.props.user.firstName}${this.props.user.lastName}/profile`}/> :
-    //                 <div className="containerUser">
-    //                     <nav className="tabBar">
-    //                         <NavLink exact activeClassName="active" to={`/authorization/company`}>COMPANY</NavLink>
-    //                         <NavLink activeClassName="active" to={`/authorization/user`}>USER</NavLink>
-    //                     </nav>
-    //                     <Route path="/authorization/company" component={AutorizationCompany}/>
-    //                     <Route path="/authorization/user" component={AutorizationUser}/>
-    //                 </div>}
     render() {
         return (
             <div>
