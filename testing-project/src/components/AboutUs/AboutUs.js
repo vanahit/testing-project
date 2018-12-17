f8236412d86e80ff24243c8bc33cba610d402b33
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2018-12-17 05:31:15 +0400

    animate numbers

--- testing-project/src/components/AboutUs/AboutUs.js
+++ testing-project/src/components/AboutUs/AboutUs.js
@@ -135,203 +135,245 @@ const TeamBlock = styled.div`
 
 
 const Box = ({boxIcon, boxItem, count}) => (
-    <BoxStyle>
-        <BoxIcon>{boxIcon}</BoxIcon>
-        <div style={{
-            borderBottom: '1px solid black',
-            width: '270px',
-            textAlign: 'center',
-            fontSize: '60px',
-            paddingBottom: '40px',
-            borderColor: 'white',
-            color: 'white',
-        }}>
-            {count ? count : <Loader/>}
-        </div>
-        <div style={{
-            color: 'white',
-            fontSize: '34px',
-            marginTop: '15px',
-        }}>
-            {boxItem}
-        </div>
-    </BoxStyle>
+   <BoxStyle>
+      <BoxIcon>{boxIcon}</BoxIcon>
+      <div style={{
+         borderBottom: '1px solid black',
+         width: '270px',
+         textAlign: 'center',
+         fontSize: '60px',
+         paddingBottom: '40px',
+         borderColor: 'white',
+         color: 'white',
+      }}>
+         {count ? count : <Loader/>}
+      </div>
+      <div style={{
+         color: 'white',
+         fontSize: '34px',
+         marginTop: '15px',
+      }}>
+         {boxItem}
+      </div>
+   </BoxStyle>
 );
 
 
 const MemberBox = ({image, name, profession}) => (
-    <div>
-        <TeamMemberImage style={{
-            background: `url(${image})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: ' center center',
-            backgroundSize: 'cover',
-        }}/>
-        <div style={{
-            textAlign: 'center',
-            margin: '16px 0 30px',
-        }}>
-            <div style={{fontSize: '24px', color: '#100529', fontWeight: 'bold', marginBottom: '8px'}}>{name}</div>
-            <div style={{fontSize: '20px', color: '#FFAD5A'}}>{profession}</div>
-        </div>
-    </div>
+   <div>
+      <TeamMemberImage style={{
+         background: `url(${image})`,
+         backgroundRepeat: 'no-repeat',
+         backgroundPosition: ' center center',
+         backgroundSize: 'cover',
+      }}/>
+      <div style={{
+         textAlign: 'center',
+         margin: '16px 0 30px',
+      }}>
+         <div style={{fontSize: '24px', color: '#100529', fontWeight: 'bold', marginBottom: '8px'}}>{name}</div>
+         <div style={{fontSize: '20px', color: '#FFAD5A'}}>{profession}</div>
+      </div>
+   </div>
 );
 
 class AboutUs extends React.Component {
 
-    state = {
-        show: false,
-        members: [
-            {
-                name: 'Anahit',
-                image: anahit,
-                profession: 'JavaScript Developer',
+   state = {
+      show: false,
+      members: [
+         {
+            name: 'Anahit',
+            image: anahit,
+            profession: 'JavaScript Developer',
 
-                start: -200,
-                delay: 2000,
-            },
-            {
-                name: 'Hayk',
-                image: hayk,
-                profession: 'JavaScript Developer',
+            start: -200,
+            delay: 2000,
+         },
+         {
+            name: 'Hayk',
+            image: hayk,
+            profession: 'JavaScript Developer',
 
-                start: -200,
-                delay: 1400,
-            },
-            {
-                name: 'Mkrtich',
-                image: mkrtich,
-                profession: 'JavaScript Developer',
+            start: -200,
+            delay: 1400,
+         },
+         {
+            name: 'Mkrtich',
+            image: mkrtich,
+            profession: 'JavaScript Developer',
 
-                start: -200,
-                delay: 800,
-            },
-            {
-                name: 'Davit',
-                image: davit,
-                profession: 'UI/UX Designer',
+            start: -200,
+            delay: 800,
+         },
+         {
+            name: 'Davit',
+            image: davit,
+            profession: 'UI/UX Designer',
 
-                start: -200,
-                delay: 200,
-            },
-        ]
-    };
+            start: -200,
+            delay: 200,
+         },
+      ],
+      testStart: 0,
+      companyStart: 0,
+      userStart: 0,
+   };
 
+   componentDidUpdate(prevProps, prevState, snapshot) {
 
-    showMembers = () => (
-        this.state.members.map((member, i) => (
-            <Animate
-                key={i}
-                show={this.state.show}
+      setTimeout(() => {
+         this.showTest();
+         this.showCompany();
+         this.showUser();
+      }, 100)
+   }
 
-                start={{
-                    opacity: 0,
-                    left: member.start,
-                    rotate: 0,
-                }}
-                enter={{
-                    opacity: [1],
-                    left: [0],
-                    rotate: [360],
-                    timing: {delay: member.delay, duration: 1000, ease: easePolyOut}
-                }}
-            >
-                {({left, opacity}) => {
-                    return (
-                        <div style={{
-                            opacity,
-                            transform: `translate(${left}px`
-                        }}>
-                            <MemberBox
-                                key={i}
-                                name={member.name}
-                                profession={member.profession}
-                                image={member.image}
+   showTest = () => {
+      if (this.state.testStart < this.props.tests) {
 
-                            />
-                        </div>
+         this.setState({
+            testStart: this.state.testStart + 1,
+         })
+      }
+   };
+   showCompany = () => {
+      if (this.state.companyStart < this.props.companies) {
 
-                    )
-                }}
+         this.setState({
+            companyStart: this.state.companyStart + 1,
+         })
+      }
+   };
+   showUser = () => {
+      if (this.state.userStart < this.props.users) {
 
-            </Animate>
-
-        ))
-    );
-
-    render() {
-        return (
-            <div>
-                <Main>
-                    <Wrapper>
-                        <AboutCheckme>ABOUT CHECK ME</AboutCheckme>
-
-                        <Description>
-                            Our main goal is cooperation, which we founded between our users and companies .We give
-                            opportunity
-                            to our users to make achievements by completing tests which created by our companies . It is
-                            important for us to create reliable collaboration. The best achievement will be for our
-                            users ,
-                            to
-                            receive invitation from leading companies. For our companies the best achievement will be
-                            invitation
-                            the best users to their company. <span
-                            style={{color: '#FFAD5A'}}>DESTINATION WAS ACHIEVES.</span>
-                        </Description>
-
-                        <Zoom>
-                            <BoxWrapper>
-                                <Box boxIcon={<TestsSvg />} count={this.props.tests} boxItem={'TESTS'}/>
-                                <Box boxIcon={<CompaniesSvg />}count={this.props.companies} boxItem={'COMPANIES'}/>
-                                <Box boxIcon={<UsersSvg/>}count={this.props.users} boxItem={'USERS'}/>
-                            </BoxWrapper>
-                        </Zoom>
-
-                    </Wrapper>
-                </Main>
-                <TeamBlock>
-                    <OurTeam>
-                        <span>OUR TEAM</span>
-                    </OurTeam>
+         this.setState({
+            userStart: this.state.userStart + 1,
+         })
+      }
+   };
 
 
-                    <Reveal
-                        fraction={1}
-                        onReveal={() => {
-                            this.setState({show: true})
-                        }}
-                    >
-                        <MemberBoxWrapper>
-                            {this.showMembers()}
-                        </MemberBoxWrapper>
-                    </Reveal>
+   showMembers = () => (
+      this.state.members.map((member, i) => (
+         <Animate
+            key={i}
+            show={this.state.show}
+
+            start={{
+               opacity: 0,
+               left: member.start,
+               rotate: 0,
+            }}
+            enter={{
+               opacity: [1],
+               left: [0],
+               rotate: [360],
+               timing: {delay: member.delay, duration: 1000, ease: easePolyOut}
+            }}
+         >
+            {({left, opacity}) => {
+               return (
+                  <div style={{
+                     opacity,
+                     transform: `translate(${left}px`
+                  }}>
+                     <MemberBox
+                        key={i}
+                        name={member.name}
+                        profession={member.profession}
+                        image={member.image}
+
+                     />
+                  </div>
+
+               )
+            }}
+
+         </Animate>
+      ))
+   );
+
+   render() {
+      return (
+         <div>
+            <Main>
+               <Wrapper>
+                  <AboutCheckme>ABOUT CHECK ME</AboutCheckme>
+
+                  <Description>
+                     Our main goal is cooperation, which we founded between our users and companies .We give
+                     opportunity
+                     to our users to make achievements by completing tests which created by our companies . It is
+                     important for us to create reliable collaboration. The best achievement will be for our
+                     users ,
+                     to
+                     receive invitation from leading companies. For our companies the best achievement will be
+                     invitation
+                     the best users to their company. <span
+                     style={{color: '#FFAD5A'}}>DESTINATION WAS ACHIEVES.</span>
+                  </Description>
+
+                  <Zoom
+                     onReveal={() => {
+                        this.showTest();
+                        this.showCompany();
+                        this.showUser();
+                     }}
+                  >
+                     <BoxWrapper>
+                        <Box boxIcon={<TestsSvg/>} count={this.state.testStart} boxItem={'TESTS'}/>
+                        <Box boxIcon={<CompaniesSvg/>} count={this.state.companyStart} boxItem={'COMPANIES'}/>
+                        <Box boxIcon={<UsersSvg/>} count={this.state.userStart} boxItem={'USERS'}/>
+                     </BoxWrapper>
+                  </Zoom>
+
+               </Wrapper>
+            </Main>
+            <TeamBlock>
+               <OurTeam>
+                  <span>OUR TEAM</span>
+               </OurTeam>
 
 
-                    <div style={{
-                        maxWidth: '1200px', margin: '0 auto 76px', color: '#100529', fontSize: '16px',
-                    }}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
-                        and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
-                        leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
-                        with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                    </div>
-                </TeamBlock>
+               <Reveal
+                  fraction={1}
+                  onReveal={() => {
+                     this.setState({show: true})
+                  }}
+               >
+                  <MemberBoxWrapper>
+                     {this.showMembers()}
+                  </MemberBoxWrapper>
+               </Reveal>
 
-            </div>
 
-        );
-    }
+               <div style={{
+                  maxWidth: '1200px', margin: '0 auto 76px', color: '#100529', fontSize: '16px',
+               }}>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                  industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
+                  and scrambled it to make a type specimen book. It has survived not only five centuries, but also the
+                  leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
+                  with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                  publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+               </div>
+            </TeamBlock>
+
+         </div>
+
+      );
+   }
 }
 
 
 function mapStateToProps(state) {
-    return {
-        tests: state.appReducer.tests.length,
-        companies: state.appReducer.companies.length,
-        users: state.appReducer.users.length,
-    }
+   return {
+      tests: state.appReducer.tests.length,
+      companies: state.appReducer.companies.length,
+      users: state.appReducer.users.length,
+   }
 
 }
 
